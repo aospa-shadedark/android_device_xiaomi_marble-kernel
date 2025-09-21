a8e87eca76a4d8179ba8bdc5a5a477b509a378f7
ShadeDark6 <nizamfa6@gmail.com>
2025-09-21 07:51:00 +0000

    marble-kernel: Sync prebuilt kernel with latest kernel source

---
+++ kernel-headers/linux/.power_state.h.cmd
@@ -0,0 +1,1 @@
+cmd_usr/include/linux/power_state.h := sh ../scripts/headers_install.sh ../include/uapi/linux/power_state.h usr/include/linux/power_state.h
---
+++ kernel-headers/media/.ispv3_defs.h.cmd
@@ -0,0 +1,1 @@
+cmd_usr/include/media/ispv3_defs.h := sh ../scripts/headers_install.sh ../include/uapi/media/ispv3_defs.h usr/include/media/ispv3_defs.h
